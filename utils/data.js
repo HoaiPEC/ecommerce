5902056fe9faaa282a3c40101f3ff71a2d1802ec
Peter Nguyen <peter@outcubator.com>
2022-09-16 21:51:31 +0700

    list product

---
+++ utils/data.js
@@ -0,0 +1,59 @@
+const data = {
+    products: [
+        {
+            'name': 'Free Shirt',
+            'category': 'Shirts',
+            'image': '/image/shirt1.jpg',
+            'price': 70,
+            'brand': 'Nike',
+            'rating': 4.5,
+            'numReviews': 10,
+            'countInStock': 100,
+            'description': 'A popular shirt'
+        },
+        {
+            'name': 'Free Shirt',
+            'category': 'Shirts',
+            'image': '/image/shirt1.jpg',
+            'price': 70,
+            'brand': 'Nike',
+            'rating': 4.5,
+            'numReviews': 10,
+            'countInStock': 100,
+            'description': 'A popular shirt'
+        },
+        {
+            'name': 'Free Shirt',
+            'category': 'Shirts',
+            'image': '/image/shirt1.jpg',
+            'price': 70,
+            'brand': 'Nike',
+            'rating': 4.5,
+            'numReviews': 10,
+            'countInStock': 100,
+            'description': 'A popular shirt'
+        },
+        {
+            'name': 'Free Shirt',
+            'category': 'Shirts',
+            'image': '/image/shirt1.jpg',
+            'price': 70,
+            'brand': 'Nike',
+            'rating': 4.5,
+            'numReviews': 10,
+            'countInStock': 100,
+            'description': 'A popular shirt'
+        },
+        {
+            'name': 'Free Shirt',
+            'category': 'Shirts',
+            'image': '/image/shirt1.jpg',
+            'price': 70,
+            'brand': 'Nike',
+            'rating': 4.5,
+            'numReviews': 10,
+            'countInStock': 100,
+            'description': 'A popular shirt'
+        },
+    ],
+};
